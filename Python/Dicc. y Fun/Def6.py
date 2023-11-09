78b6bafc61b8115463237a24d388419790dfaf2e
ElMati-01 <129073952+ElMati-01@users.noreply.github.com>
2023-11-09 00:07:21 -0500

    Update Def6.py

--- Python/Dicc. y Fun/Def6.py
+++ Python/Dicc. y Fun/Def6.py
@@ -1,4 +1,5 @@
 
+# Aquí a traves de una variable se asignan parametros en el cual x e y equivalen a "X" and "Y"
 
 operadorand = lambda x, y: x & y
 
@@ -8,3 +9,5 @@ for i in range(2):
 
         print(f"{i} & {j} = {operadorand(i, j)}")
 
+#  Aquí se está haciendo una especie de caulciladora de numeros binarios, los valores se impriminen hasta 2 gracias al for.
+# el for itera sobre los números del 0 al 1 . La variable j se utiliza para almacenar el número actual en el bucle.
