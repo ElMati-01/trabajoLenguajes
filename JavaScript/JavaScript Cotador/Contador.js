8e64aa08a211141ad2164b50baa8a54bdda7fdb0
ElMati-01 <129073952+ElMati-01@users.noreply.github.com>
2023-11-28 23:14:14 -0500

    Contador tarea js

---
+++ JavaScript/JavaScript Cotador/Contador.js
@@ -0,0 +1,84 @@
+let contador1= 0
+let contador2 = 0
+const valor= document.getElementById("valor")
+const valor1= document.getElementById("valor1")
+
+window.addEventListener("keydown", (e)=> {
+
+    if (e.key == "+"){
+        incremento()
+
+    }
+
+    if(e.key == "-"){
+        decremento()
+
+    }
+}
+);
+
+
+
+
+function incremento(){
+
+    contador1+=1
+    valor.innerHTML=contador1 * 39900
+}
+
+function decremento(){
+
+    if (contador1>0){
+    contador1-=1
+    valor.innerHTML=contador1 * 39900
+    }
+
+    else{
+        contador1=0
+        valor.innerHTML=contador1
+    }
+}
+
+
+
+function resetear(){
+
+    contador1=0
+    contador2 = 0
+    valor.innerHTML=contador1
+    valor1.innerHTML= contador2
+
+}
+
+
+window.addEventListener("keydown", (e)=> {
+
+    if (e.key == "+"){
+        incremento1()
+    }
+
+    if(e.key == "-"){
+        decremento1()
+    }
+}
+);
+
+
+function incremento1(){
+
+    contador2+=1
+    valor1.innerHTML=contador2 * 56900
+}
+
+function decremento1(){
+
+    if (contador2>0){
+    contador2-=1
+    valor1.innerHTML=contador2 * 56900
+    }
+
+    else{
+        contador2=0
+        valor1.innerHTML=contador2
+    }
+}
