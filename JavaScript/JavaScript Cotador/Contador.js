85ae492b8fe4d88ddd2713a81210b5e930a1fddb
ElMati-01 <129073952+ElMati-01@users.noreply.github.com>
2023-11-29 01:08:39 -0500

    Libreria

--- JavaScript/JavaScript Cotador/Contador.js
+++ JavaScript/JavaScript Cotador/Contador.js
@@ -1,8 +1,13 @@
+
+// Linea de contadores en 0 para operaciones 
 let contador1= 0
 let contador2 = 0
+
+// Se obtiene los "valores" de los botones de HTML
 const valor= document.getElementById("valor")
 const valor1= document.getElementById("valor1")
 
+// Se crea ina función de escucha para las funciones
 window.addEventListener("keydown", (e)=> {
 
     if (e.key == "+"){
@@ -18,7 +23,7 @@ window.addEventListener("keydown", (e)=> {
 );
 
 
-
+// Función para incrementar contador
 
 function incremento(){
 
@@ -26,6 +31,7 @@ function incremento(){
     valor.innerHTML=contador1 * 39900
 }
 
+// Función para decrementar contador (Se multiplica en vez de dividirse. La multiplicación del precio se agrega en este caso a "contador1" para que el contador reste todos los nuemros por el precio indicado)
 function decremento(){
 
     if (contador1>0){
@@ -41,16 +47,8 @@ function decremento(){
 
 
 
-function resetear(){
 
-    contador1=0
-    contador2 = 0
-    valor.innerHTML=contador1
-    valor1.innerHTML= contador2
-
-}
-
-
+// Se crea ina función de escucha para las funciones (Caso exculsivo para las funciones y variables con 1)
 window.addEventListener("keydown", (e)=> {
 
     if (e.key == "+"){
@@ -63,13 +61,15 @@ window.addEventListener("keydown", (e)=> {
 }
 );
 
-
+// Función para incrementar contador
 function incremento1(){
 
     contador2+=1
     valor1.innerHTML=contador2 * 56900
 }
 
+
+// Función para decrementar contador
 function decremento1(){
 
     if (contador2>0){
@@ -82,3 +82,30 @@ function decremento1(){
         valor1.innerHTML=contador2
     }
 }
+
+
+// Función para resetear contadores
+function resetear(){
+
+    contador1=0
+    contador2 = 0
+    valor.innerHTML=contador1
+    valor1.innerHTML= contador2
+
+}
+
+function calcularTotal(){
+
+    const precioTotal= (contador1 * 39900) + (contador2 * 56900);
+
+    const resultado = document.getElementById("resultado");
+    resultado.innerHTML = `Total: ${precioTotal}`;
+    
+}
+
+function Mostrar() {
+    calcularTotal();
+  
+    const respuesta = document.getElementById("respuesta");
+    respuesta.style.display = "block";
+  }
